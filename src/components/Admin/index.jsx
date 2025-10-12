526b7e1a4e59780b27d85a4f6ff7e0b43222f180
raushan <kraushan022@gmail.com>
2025-10-12 18:21:22 +0530

    admin dropdown ui

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 const Admin = () => {
   const [sidebarSize, setSidebarSize] = useState(280);
+  const [openAccountMenu, setOpenAccountMenu] = useState(false);
+
   return (
     <div>
       <aside
@@ -15,7 +17,7 @@ const Admin = () => {
           transition: "0.3s",
         }}
       >
-        <nav className="bg-white p-6 shadow">
+        <nav className="bg-white p-6 shadow  flex items-center justify-between">
           <div className="flex gap-4 items-center">
             <button
               onClick={() => setSidebarSize(sidebarSize === 280 ? 0 : 280)}
@@ -24,6 +26,30 @@ const Admin = () => {
               <i className="ri-menu-2-line text-xl"></i>
             </button>
             <h1 className="text-md font-semibold">shopcode</h1>
+          </div>
+
+          <div>
+            <button className="relative">
+              <img
+                src="./images/avt.jpg"
+                alt="avatar"
+                className="w-12 h-12 rounded-full hover:cursor-pointer"
+                onClick={() => setOpenAccountMenu(!openAccountMenu)}
+              />
+              {openAccountMenu && (
+                <div className="absolute top-[76px] right-0 bg-white w-[200px] p-6 shadow-lg">
+                  <div>
+                    <h1 className="text-lg font-semibold">Er Raushan</h1>
+                    <p className="text-gray-500">example@gmail.com</p>
+                    <div className="h-px bg-gray-200 my-4"></div>
+                    <button className="hover:cursor-pointer">
+                      <i className="ri-logout-circle-r-line mr-2"></i>
+                      Logout
+                    </button>
+                  </div>
+                </div>
+              )}
+            </button>
           </div>
         </nav>
       </section>
