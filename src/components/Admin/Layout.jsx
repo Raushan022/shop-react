be28f27ddebe01a1793a31a60ad9e2dd62b3aee9
raushan <kraushan022@gmail.com>
2025-10-13 12:15:26 +0530

    added layout component menus and mobile as well web view

--- src/components/Admin/Layout.jsx
+++ src/components/Admin/Layout.jsx
@@ -1,61 +1,197 @@
 import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
 
 const Layout = ({ children }) => {
-  const [sidebarSize, setSidebarSize] = useState(280);
-  const [openAccountMenu, setOpenAccountMenu] = useState(false);
+  const [size, setSize] = useState(280);
+  const [mobileSize, setMobileSize] = useState(0);
+  const [accountMenu, setAccountMenu] = useState(false);
+
+  const menus = [
+    {
+      sno: 1,
+      label: "Dashboard",
+      icon: <i className="ri-dashboard-3-line mr-2"></i>,
+      link: "/admin/dashboard",
+    },
+    {
+      sno: 2,
+      label: "Products",
+      icon: <i className="ri-shopping-cart-line mr-2"></i>,
+      link: "/admin/products",
+    },
+    {
+      sno: 3,
+      label: "Orders",
+      icon: <i className="ri-shape-line mr-2"></i>,
+      link: "/admin/orders",
+    },
+    {
+      sno: 4,
+      label: "Payments",
+      icon: <i className="ri-money-dollar-circle-line mr-2"></i>,
+      link: "/admin/payments",
+    },
+
+    {
+      sno: 5,
+      label: "Settings",
+      icon: <i className="ri-settings-3-line mr-2"></i>,
+      link: "/admin/settings",
+    },
+    {
+      sno: 6,
+      label: "Logout",
+      icon: <i className="ri-logout-circle-r-line mr-2"></i>,
+      link: "/admin/logout",
+    },
+  ];
 
   return (
-    <div className="relative">
-      <aside
-        className="fixed top-0 left-0 h-full bg-indigo-600"
-        style={{ width: sidebarSize, transition: "0.3s" }}
-      ></aside>
-      <section
-        className="bg-gray-100 h-screen"
-        style={{
-          marginLeft: sidebarSize,
-          transition: "0.3s",
-        }}
-      >
-        <nav className="bg-white p-6 shadow  flex items-center justify-between sticky top-0 left-0">
-          <div className="flex gap-4 items-center">
-            <button
-              onClick={() => setSidebarSize(sidebarSize === 280 ? 0 : 280)}
-              className="bg-gray-100 hover:bg-indigo-500 hover:text-white w-8 h-8"
-            >
-              <i className="ri-menu-2-line text-xl"></i>
-            </button>
-            <h1 className="text-md font-semibold">shopcode</h1>
+    <>
+      {/* Desktop */}
+      <div className="md:block hidden">
+        <aside
+          className="bg-indigo-600 fixed top-0 left-0 h-full overflow-hidden"
+          style={{
+            width: size,
+            transition: "0.3s",
+          }}
+        >
+          <div className="flex flex-col">
+            {menus.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.link}
+                // className="px-4 py-3 text-gray-100 text-[17.5px] hover:bg-rose-500 hover:text-white"
+                className={({ isActive }) =>
+                  `px-4 py-3 text-gray-100 text-[17.5px] hover:bg-rose-500 hover:text-white ${
+                    isActive ? "bg-yellow-600" : ""
+                  }`
+                }
+              >
+                {item.icon}
+                {item.label}
+              </NavLink>
+            ))}
           </div>
+        </aside>
+        <section
+          className="bg-gray-100 h-screen"
+          style={{
+            marginLeft: size,
+            transition: "0.3s",
+          }}
+        >
+          <nav className="bg-white p-6 shadow flex items-center justify-between sticky top-0 left-0">
+            <div className="flex gap-4 items-center">
+              <button
+                className="bg-gray-50 hover:bg-indigo-600 hover:text-white w-8 h-8"
+                onClick={() => setSize(size === 280 ? 0 : 280)}
+              >
+                <i className="ri-menu-2-line text-xl"></i>
+              </button>
+              <h1 className="text-md font-semibold">Shopcode</h1>
+            </div>
 
-          <div>
-            <button className="relative">
-              <img
-                src="/images/avt.jpg"
-                alt="avatar"
-                className="w-12 h-12 rounded-full hover:cursor-pointer"
-                onClick={() => setOpenAccountMenu(!openAccountMenu)}
-              />
-              {openAccountMenu && (
-                <div className="absolute top-[76px] right-0 bg-white w-[200px] p-6 shadow-lg">
-                  <div>
-                    <h1 className="text-lg font-semibold">Er Raushan</h1>
-                    <p className="text-gray-500">example@gmail.com</p>
-                    <div className="h-px bg-gray-200 my-4"></div>
-                    <button className="hover:cursor-pointer">
-                      <i className="ri-logout-circle-r-line mr-2"></i>
-                      Logout
-                    </button>
+            <div>
+              <button className="relative">
+                <img
+                  src="/images/avt.avif"
+                  className="w-10 h-10 rounded-full"
+                  onClick={() => setAccountMenu(!accountMenu)}
+                />
+                {accountMenu && (
+                  <div className="absolute top-[76px] right-0 bg-white w-[200px] p-6 shadow-lg">
+                    <div>
+                      <h1 className="text-lg font-semibold">Er Raushan</h1>
+                      <p className="text-gray-500">example@gmail.com</p>
+                      <div className="h-px bg-gray-200 my-4"></div>
+                      <button className="hover:cursor-pointer">
+                        <i className="ri-logout-circle-r-line mr-2"></i>
+                        Logout
+                      </button>
+                    </div>
                   </div>
-                </div>
-              )}
-            </button>
-          </div>
-        </nav>
+                )}
+              </button>
+            </div>
+          </nav>
+          <div className="p-6">{children}</div>
+        </section>
+      </div>
 
-        <div className="p-6">{children}</div>
-      </section>
-    </div>
+      {/* mobile */}
+      <div className="md:hidden block">
+        <aside
+          className="bg-indigo-600 fixed top-0 left-0 h-full z-50 overflow-hidden"
+          style={{
+            width: mobileSize,
+            transition: "0.3s",
+          }}
+        >
+          <div className="flex flex-col">
+            <button
+              className="text-left mx-4 mt-4"
+              onClick={() => setMobileSize(mobileSize === 0 ? 280 : 0)}
+            >
+              <i className="ri-menu-2-fill text-white text-xl"></i>
+            </button>
+            {menus.map((item, index) => (
+              <NavLink
+                key={index}
+                to={item.link}
+                // className="px-4 py-3 text-gray-100 text-[17.5px] hover:bg-rose-500 hover:text-white"
+                className={({ isActive }) =>
+                  `px-4 py-3 text-gray-100 text-[17.5px] hover:bg-rose-500 hover:text-white ${
+                    isActive ? "bg-yellow-600" : ""
+                  }`
+                }
+              >
+                {item.icon}
+                {item.label}
+              </NavLink>
+            ))}
+          </div>
+        </aside>
+        <section className="bg-gray-100 h-screen">
+          <nav className="bg-white p-6 shadow flex items-center justify-between sticky top-0 left-0">
+            <div className="flex gap-4 items-center">
+              <button
+                className="bg-gray-50 hover:bg-indigo-600 hover:text-white w-8 h-8"
+                onClick={() => setMobileSize(mobileSize === 0 ? 280 : 0)}
+              >
+                <i className="ri-menu-2-line text-xl"></i>
+              </button>
+              <h1 className="text-md font-semibold">Shopcode</h1>
+            </div>
+
+            <div>
+              <button className="relative">
+                <img
+                  src="/images/avt.avif"
+                  className="w-10 h-10 rounded-full"
+                  onClick={() => setAccountMenu(!accountMenu)}
+                />
+                {accountMenu && (
+                  <div className="absolute top-[76px] right-0 bg-white w-[200px] p-6 shadow-lg">
+                    <div>
+                      <h1 className="text-lg font-semibold">Er Raushan</h1>
+                      <p className="text-gray-500">example@gmail.com</p>
+                      <div className="h-px bg-gray-200 my-4"></div>
+                      <button className="hover:cursor-pointer">
+                        <i className="ri-logout-circle-r-line mr-2"></i>
+                        Logout
+                      </button>
+                    </div>
+                  </div>
+                )}
+              </button>
+            </div>
+          </nav>
+          <div className="p-6">{children}</div>
+        </section>
+      </div>
+    </>
   );
 };
 
