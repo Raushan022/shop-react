982da5854d13559ffd9f30bcd23c121fed22af4b
raushan <kraushan022@gmail.com>
2025-10-13 12:49:44 +0530

    added routes and components  for nav elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "remixicon/fonts/remixicon.css";
 import NotFound from "./components/NotFound";
+import Dashboard from "./components/Admin/Dashboard";
+import Customers from "./components/Admin/Customers";
 import Products from "./components/Admin/Products";
+import Payments from "./components/Admin/Payments";
+import Settings from "./components/Admin/Settings";
 import Orders from "./components/Admin/Orders";
 
 const App = () => {
@@ -9,8 +13,12 @@ const App = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/admin">
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="customers" element={<Customers />} />
           <Route path="products" element={<Products />} />
           <Route path="orders" element={<Orders />} />
+          <Route path="payments" element={<Payments />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
         <Route path="*" element={<NotFound />} />
       </Routes>
