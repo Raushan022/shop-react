375b7be2334cf6629b8e2215a1e926e6f79a3c71
raushan <kraushan022@gmail.com>
2025-10-14 15:29:23 +0530

    added swiper library in home to swipe images

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,46 @@
 import React from "react";
 import MainLayout from "./MainLayout";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 const Home = () => {
   return (
     <MainLayout>
-      <div>Home</div>
+      <div>
+        <header>
+          <Swiper
+            pagination={true}
+            navigation={true}
+            modules={[Navigation, Pagination]}
+            slidesPerView={1}
+            // onSlideChange={() => console.log("slide change")}
+            // onSwiper={(swiper) => console.log(swiper)}
+          >
+            <SwiperSlide>
+              <img src="/images/p1.jpg" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src="/images/p2.jpg" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src="/images/p3.jpg" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src="/images/p4.jpg" />
+            </SwiperSlide>
+
+            <SwiperSlide>
+              <img src="/images/p5.jpg" />
+            </SwiperSlide>
+          </Swiper>
+        </header>
+      </div>
     </MainLayout>
   );
 };
