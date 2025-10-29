b21a1de5be5b5106d264d3e4a5aff2f33cdb0062
raushan <kraushan022@gmail.com>
2025-10-29 20:19:16 +0530

    profile reload bug fix

--- src/components/MainLayout.jsx
+++ src/components/MainLayout.jsx
@@ -15,7 +15,7 @@ const MainLayout = ({ children }) => {
       if (user) {
         setSession(user);
       } else {
-        setSession(null);
+        setSession(false);
       }
     });
   }, []);
@@ -38,6 +38,17 @@ const MainLayout = ({ children }) => {
       href: "/contact-us",
     },
   ];
+
+  if (session === null)
+    return (
+      <div className="bg-gray-100 h-full fixed top-0 left-0 w-full flex justify-center items-center">
+        <span className="relative flex h-6 w-6">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-6 w-6 bg-sky-500"></span>
+        </span>
+      </div>
+    );
+
   return (
     <>
       <div>
