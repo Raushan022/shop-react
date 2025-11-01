0323b8449736c7245618c2e903306cc4d89ce885
raushan <kraushan022@gmail.com>
2025-11-01 10:49:42 +0530

    login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,34 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import firebaseAppConfig from "../utils/firebase-config";
+
+const auth = getAuth(firebaseAppConfig);
 
 const Login = () => {
+  const navigate = useNavigate();
   const [passwordType, setPasswordType] = useState("password");
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const login = async (e) => {
+    try {
+      e.preventDefault();
+      await signInWithEmailAndPassword(auth, formData.email, formData.password);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    setFormData({ ...formData, [name]: value });
+  };
 
   return (
     <div className="grid md:grid-cols-2 md:h-screen md:overflow-hidden animate__animated animate__fadeIn">
@@ -13,13 +39,14 @@ const Login = () => {
       <div className="flex flex-col md:p-16 p-8">
         <h1 className="text-4xl font-bold">Signin</h1>
         <p className="text-lg text-gray-600">Enter profile details to login</p>
-        <form className="mt-8 space-y-6">
+        <form className="mt-8 space-y-6" onSubmit={login}>
           <div className="flex flex-col">
             <label className="font-semibold text-lg mb-1">Email id</label>
             <input
               required
               type="email"
               name="email"
+              onChange={handleChange}
               placeholder="example@mail.com"
               className="p-3 border border-gray-300 rounded"
             />
@@ -31,6 +58,7 @@ const Login = () => {
               required
               type={passwordType}
               name="password"
+              onChange={handleChange}
               placeholder="********"
               className="p-3 border border-gray-300 rounded"
             />
