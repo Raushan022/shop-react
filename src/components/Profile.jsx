e2f0d0d22f6005bf382b2f2c5a5934809cb0af6b
raushan <kraushan022@gmail.com>
2025-11-24 13:53:58 +0530

    profile page

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import MainLayout from "./MainLayout";
 import firebaseAppConfig from "../utils/firebase-config";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getAuth, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { getDownloadURL, getStorage, ref, uploadBytes } from "firebase/storage";
 import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const auth = getAuth(firebaseAppConfig);
+const storage = getStorage();
 
 const Profile = () => {
   const navigate = useNavigate();
@@ -12,12 +15,7 @@ const Profile = () => {
   const [formValue, setFormValue] = useState({
     fullname: "",
     email: "",
-    mobile: "9798798",
-    address: "",
-    city: "",
-    state: "",
-    country: "",
-    pincode: "",
+    mobile: "",
   });
 
   useEffect(() => {
@@ -31,6 +29,35 @@ const Profile = () => {
     });
   }, []);
 
+  useEffect(() => {
+    if (session) {
+      setFormValue({
+        ...formValue,
+        fullname: session.displayName,
+        mobile: session.phoneNumber ? session.phoneNumber : "",
+      });
+    }
+  }, [session]);
+
+  const setProfilePicture = async (e) => {
+    const input = e.target;
+    const file = input.files[0];
+    const filenameArray = file.name.split(".");
+    const ext = filenameArray[filenameArray.length - 1];
+    const filename = Date.now() + "." + ext;
+    const path = `pictures/${filename}`;
+    const bucket = ref(storage, path);
+    const snapshot = await uploadBytes(bucket, file);
+    const url = await getDownloadURL(snapshot.ref);
+    await updateProfile(auth.currentUser, {
+      photoURL: url,
+    });
+    setSession({
+      ...session,
+      photoURL: url,
+    });
+  };
+
   const handleFormValue = (e) => {
     const input = e.target;
     const name = input.name;
@@ -39,6 +66,23 @@ const Profile = () => {
       ...formValue,
       [name]: value,
     });
+  };
+
+  const saveProfileInfo = async (e) => {
+    e.preventDefault();
+    await updateProfile(auth.currentUser, {
+      displayName: formValue.fullname,
+      phoneNumber: formValue.mobile,
+    });
+    new Swal({
+      icon: "success",
+      title: "Profile Saved!!",
+    });
+  };
+
+  const setAddress = (e) => {
+    e.preventDefault();
+    alert("kjnjn");
   };
 
   return (
@@ -57,10 +101,11 @@ const Profile = () => {
             type="file"
             accept="image/*"
             className="opacity-0 absolute top-0 left-0 w-full h-full"
+            onChange={setProfilePicture}
           />
         </div>
 
-        <form className="grid grid-cols-2 gap-6">
+        <form className="grid grid-cols-2 gap-6" onSubmit={saveProfileInfo}>
           <div className="flex flex-col gap-2">
             <label className="text-lg font-semibold">Fullname</label>
             <input
@@ -68,13 +113,14 @@ const Profile = () => {
               name="fullname"
               className="p-2 rounded border border-gray-300"
               onChange={handleFormValue}
-              value={session?.displayName || ""}
+              value={formValue.fullname}
             />
           </div>
 
           <div className="flex flex-col gap-2">
             <label className="text-lg font-semibold">Email</label>
             <input
+              disabled
               required
               name="email"
               type="email"
@@ -98,6 +144,22 @@ const Profile = () => {
 
           <div />
 
+          <button className="px-4 py-2 bg-rose-600 text-white rounded w-fit hover:bg-green-600">
+            <i className="ri-save-line mr-2"></i>
+            Save
+          </button>
+        </form>
+      </div>
+
+      <div className="mx-auto md:my-16 shadow-lg rounded-md p-8 md:w-7/12 border">
+        <div className="flex gap-3">
+          <i className="ri-link-unlink-m text-4xl"></i>
+          <h1 className="text-3xl font-semibold">Delivery Address</h1>
+        </div>
+
+        <hr className="my-6" />
+
+        <form className="grid grid-cols-2 gap-6" onSubmit={setAddress}>
           <div className="flex flex-col gap-2 col-span-2">
             <label className="text-lg font-semibold">Area/Street/Vill</label>
             <input
@@ -157,6 +219,7 @@ const Profile = () => {
               value={formValue.pincode}
             />
           </div>
+
           <button className="px-4 py-2 bg-rose-600 text-white rounded w-fit hover:bg-green-600">
             <i className="ri-save-line mr-2"></i>
             Save
